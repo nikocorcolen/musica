580e3ff4ff2fb6adc7bca9eed5e1992432d3b5a5
Nicolas Maturana <nikocorcolen@gmail.com>
2014-10-09 21:55:10 -0300

    Arreglo para que los grupos se creen, con token caduco

--- app/models/grupo.rb
+++ app/models/grupo.rb
@@ -12,20 +12,27 @@ class Grupo < ActiveRecord::Base
 	end
 
 	def graph
-		token = "CAACEdEose0cBAOWAcsaPF0lQ6vS9h9JuOvPZA6hN6jwfiooZBfbNkOmK1RshE2SWYeHxETrmCdjarKdl51Y7EZAG75IywVRT14xLFqk0Cv2XNcpiSCAn7nucGZBVyyC6R8qqbubsfdZCqlghCZCSEHGVqTxZA0ZAFM9YlBkyeO41udetZBQhuKWC5D8QJpxZC251xd7X3VBImz7D9ZADn6p9zIqDdfKB50h3PUZD"
 		graph = Koala::Facebook::GraphAPI.new
 		begin
 			#Todos los likes
 			self.like = graph.get_object(self.nombre)["likes"]
 			#Likes por Pais
-			response = HTTParty.get("https://graph.facebook.com/#{self.nombre}/insights/page_fans_country?access_token="+token, :query => {:oauth_token => token})
-			json = JSON.parse(response.body)
-    		self.like_pais = json['data'][0]['values'][0]['value']['CL']
+			likesPais
 			true
 		rescue
 			validar
 			false
-		ensure
+		end
+	end
+
+	def likesPais
+		begin
+			token = "CAACEdEose0cBAOWAcsaPF0lQ6vS9h9JuOvPZA6hN6jwfiooZBfbNkOmK1RshE2SWYeHxETrmCdjarKdl51Y7EZAG75IywVRT14xLFqk0Cv2XNcpiSCAn7nucGZBVyyC6R8qqbubsfdZCqlghCZCSEHGVqTxZA0ZAFM9YlBkyeO41udetZBQhuKWC5D8QJpxZC251xd7X3VBImz7D9ZADn6p9zIqDdfKB50h3PUZD"
+			response = HTTParty.get("https://graph.facebook.com/#{self.nombre}/insights/page_fans_country?access_token="+token, :query => {:oauth_token => token})
+			json = JSON.parse(response.body)
+			self.like_pais = json['data'][0]['values'][0]['value']['CL']
+		rescue Exception => e
+			self.like_pais = 0
 		end
 	end
 end
